57799ee5808afeca372622538adec46d488f4ad3
mahcloud <mike.hoffert@gmail.com>
2015-06-26 20:06:13 -0600

    Fix 500 error on emails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "emails#new"
+  root to: "books#index"
 
+  resources :books, only: [:index]
   resources :emails, only: [:new, :create]
   resources :images, only: [:create]
 end
