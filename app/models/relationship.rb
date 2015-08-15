0cc1095520a0110399c88b2543847473f75aa5f7
Mike Hoffert <mike.hoffert@gmail.com>
2015-08-15 13:48:32 -0600

    Don't duplicate relationships for couples that are both descendants

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,34 +4,32 @@ class Relationship < ActiveRecord::Base
   end
 
   def self.import(csv)
-    relationships = []
     csv.each do |row|
-      relationships << self.import_row(row) if !row['KBN'].blank? && !row['order'].blank?
+      self.import_row(row) if !row['KBN'].blank? && !row['order'].blank?
     end
-    relationships
   end
 
   def self.import_row(row)
-    relationship = nil
     person = Person.find_by_kbn(row['KBN'])
     partner = Person.find_by_kbn(row['SKBN']) if row['SKBN'].present?
     partner = Person.create({first_name: row['first_name'], last_names: [row['last_name']]}) if partner.nil?
     partner.update_attributes({birth_day: row['birth_day'], death_day: row['death_day']})
     if !person.nil? && !partner.nil?
-      relationship = Relationship.create
-      relationship.partner_ids << person.id
-      relationship.partner_ids << partner.id
-      relationship.marriage_day = row['marriage_day'] if !row['married'].blank? && !row['marriage_day'].blank?
-      relationship.divorce_day = row['divorce_day'] if row['married'] == '**'
-      relationship.partner_ids_will_change!
-      relationship.save
-      person.relationship_ids << relationship.id
-      person.relationship_ids_will_change!
-      person.save
-      partner.relationship_ids << relationship.id
-      partner.relationship_ids_will_change!
-      partner.save
+      if !Relationship.where.contains(partner_ids: [person.id, partner.id]).any?
+        relationship = Relationship.create
+        relationship.partner_ids << person.id
+        relationship.partner_ids << partner.id
+        relationship.marriage_day = row['marriage_day'] if !row['married'].blank? && !row['marriage_day'].blank?
+        relationship.divorce_day = row['divorce_day'] if row['married'] == '**'
+        relationship.partner_ids_will_change!
+        relationship.save
+        person.relationship_ids << relationship.id
+        person.relationship_ids_will_change!
+        person.save
+        partner.relationship_ids << relationship.id
+        partner.relationship_ids_will_change!
+        partner.save
+      end
     end
-    relationship
   end
 end
